cadbba121c02a96ef4cd52b7ccfadf4e72893cbb
jackcamp <jackcamp@vt.edu>
2020-04-29 23:06:36 -0400

    Added a red sphere

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ from color import Color
 from ray import Ray
 
 
+def hit_sphere(center, radius, ray):
+
+    oc = ray.origin - center
+    a = ray.direction.dot(ray.direction)
+    b = 2.0 * oc.dot(ray.direction)
+    c = oc.dot(oc) - radius * radius
+    discriminant = b*b - 4.0*a*c
+    return (discriminant > 0)
+
+
 def ray_color(ray):
+
+    if hit_sphere(Vec3([0, 0, -1]), 0.5, ray):
+        return Color([1, 0, 0])
 
     unit_direction = ray.direction.norm()
     t = 0.5 * (unit_direction.y + 1.0)
@@ -23,7 +36,6 @@ vertical = Vec3([0.0, 2.0, 0.0])
 origin = Vec3([0.0, 0.0, 0.0])
 
 for j in range(image_height - 1):
-    print("{} / {}".format(j + 1, image_height), end="\r")
     for i in range(image_width - 1):
 
         u = i / image_width
@@ -33,5 +45,7 @@ for j in range(image_height - 1):
 
         color = ray_color(r)
         pixels[i, image_height - j - 1] = tuple(color)
+
+    print("{} / {}".format(j + 1, image_height), end="\r")
 
 img.show()
