bf0b1f23ad9fef1eadf21751e81073f6a202c0fa
jackcamp <jackcamp@vt.edu>
2020-04-30 00:11:16 -0400

    Added a method for setting the face normal of a HitRecord

--- hittable.py
+++ hittable.py
@@ -2,9 +2,18 @@ from vec3 import Vec3
 
 
 class HitRecord(object):
-    t = 0.0
+
     position = Vec3()
     normal = Vec3()
+    t = 0.0
+    frontFacing = False
+
+    def setFaceNormal(self, ray, outwardNormal):
+        self.frontFacing = ray.direction.dot(outwardNormal) < 0
+        if self.frontFacing:
+            self.normal = outwardNormal
+        else:
+            self.normal = -outwardNormal
 
 
 class Hittable(object):
