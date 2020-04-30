7350963b1084ff7248814b912a00d5d8534c29c1
JacksonCampolattaro <jackcamp@vt.edu>
2020-04-29 22:39:18 -0400

    Preparing for raycasted gradient

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from PIL import Image
+from vec3 import Vec3
 from color import Color
 from ray import Ray
 
 
 def ray_color(ray):
-    unit_direction = ray.norm()
+
+    print(ray.direction)
+    unit_direction = Vec3([0, 1, 0])
     t = 0.5 * (unit_direction.y + 1)
     return (1.0 - t) * Color([1, 1, 1]) + t * Color([0.5, 0.7, 1.0])
 
@@ -15,10 +18,21 @@ image_width = 2000
 img = Image.new("RGB", (image_width, image_height), "black")
 pixels = img.load()
 
+lower_left_corner = Vec3([-2.0, -1.0, -1.0])
+horizontal = Vec3([4.0, 0.0, 0.0])
+vertical = Vec3([4.0, 0.0, 0.0])
+origin = Vec3([0.0, 0.0, 0.0])
+
 for j in range(image_height - 1):
     print("{} / {}".format(j + 1, image_height), end="\r")
     for i in range(image_width - 1):
-        c = Color([i / image_width, j / image_height, 0.2])
-        pixels[i, image_height - j - 1] = tuple(c)
+
+        u = i / image_width
+        v = i / image_height
+
+        r = Ray(origin, lower_left_corner + u * horizontal + v * vertical)
+
+        color = ray_color(r)
+        pixels[i, image_height - j - 1] = tuple(color)
 
 img.show()
