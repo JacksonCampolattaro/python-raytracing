04097f84c272dd725305512176558bbefb18c1fc
jackcamp <jackcamp@vt.edu>
2020-04-30 12:31:20 -0400

    Added a bit of parallelism

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from camera import Camera
 from PIL import Image
 import random
 
+from functools import partial
+from multiprocessing import Pool
+
 
 def ray_color(ray, world):
 
@@ -36,23 +39,38 @@ world = HittableList()
 world.objects.append(Sphere([0, 0, -1], 0.5))
 world.objects.append(Sphere([0, -100.5, -1], 100))
 
+
+def drawPixel(j, i):
+
+    color = Color([0.0, 0.0, 0.0])
+
+    for _ in range(samples_per_pixel):
+
+        u = (i + random.random()) / image_width
+        v = (j + random.random()) / image_height
+
+        ray = camera.getRay(u, v)
+
+        color += ray_color(ray, world)
+
+    color /= samples_per_pixel
+    # pixels[i, image_height - j - 1] = tuple(color)
+    return color
+
+
+def drawRow(j):
+
+    with Pool() as pool:
+
+        func = partial(drawPixel, j)
+        colors = pool.map(func, range(image_width - 1))
+
+        for i in range(image_width - 1):
+            pixels[i, image_height - j - 1] = tuple(colors[i])
+
+
 for j in range(image_height - 1):
-    for i in range(image_width - 1):
-
-        color = Color([0.0, 0.0, 0.0])
-
-        for _ in range(samples_per_pixel):
-
-            u = (i + random.random()) / image_width
-            v = (j + random.random()) / image_height
-
-            ray = camera.getRay(u, v)
-
-            color += ray_color(ray, world)
-
-        color /= samples_per_pixel
-        pixels[i, image_height - j - 1] = tuple(color)
-
+    drawRow(j)
     print("{} / {}".format(j + 1, image_height), end="\r")
 
 img.show()
