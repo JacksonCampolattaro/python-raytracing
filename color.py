6bef67c5333719decca376b57d490428db871cbb
JacksonCampolattaro <jackcamp@vt.edu>
2020-04-29 20:05:47 -0400

    Color can now be implicitly cast to a tuple of integers

---
+++ color.py
@@ -0,0 +1,24 @@
+import numpy
+
+
+class Color(numpy.ndarray):
+
+    def __new__(cls, input_array=(0, 0, 0)):
+        obj = numpy.asarray(input_array).view(cls)
+        return obj
+
+    @property
+    def r(self):
+        return self[0]
+
+    @property
+    def g(self):
+        return self[1]
+
+    @property
+    def b(self):
+        return self[2]
+
+    def __iter__(self):
+        for x in numpy.nditer(self):
+            yield int(255 * x.item())
--- ray.py
+++ ray.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from vec3 import Vec3
+from color import Color
 
 
 imageHeight = 250
@@ -9,9 +10,9 @@ img = Image.new("RGB", (imageHeight, imageWidth), "black")
 pixels = img.load()
 
 for y in range(img.size[0]):
-    print("Rendering column ", y, end="\r")
+    # print("Rendering column ", y, end="\r")
     for x in range(img.size[1]):
-        color = Vec3([x / imageWidth, y / imageHeight, 0.2])
+        color = Color([x / imageWidth, y / imageHeight, 0.2])
         pixels[y, x] = tuple(color)
 
 
