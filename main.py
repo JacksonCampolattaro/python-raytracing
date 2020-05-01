2d0840df590ddef8531a38fdcbdc33d3df4831ca
jackcamp <jackcamp@vt.edu>
2020-04-30 22:42:22 -0400

    Set up a pretty scene, for testing

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ import random
 import multiprocessing
 
 
-image_height = 200
-image_width = 300
-samples_per_pixel = 40
-max_depth = 10
+image_height = 400
+image_width = 500
+samples_per_pixel = 20
+max_depth = 4
 
 img = Image.new("RGB", (image_width, image_height), "black")
 pixels = img.load()
 
 camera = Camera(
-    Vec3(0.0, 0.0, 1.0),
+    Vec3(3.0, 1.0, 2.0),
     Vec3(0.0, 0.0, -1.0),
     Vec3(0.0, 1.0, 0.0),
     70,
@@ -30,10 +30,10 @@ camera = Camera(
 
 world = HittableList()
 
-world.objects.append(Sphere(Vec3(0, 0, -1), 0.5, Lambertian(Vec3(0.7, 0.3, 0.3))))
-world.objects.append(Sphere(Vec3(0, -100.5, -1), 100, Lambertian(Vec3(0.8, 0.8, 0.0))))
-world.objects.append(Sphere(Vec3(1.1, 0, -1), 0.5, Metal(Vec3(0.8, 0.8, 0.8), 0.1)))
-world.objects.append(Sphere(Vec3(-1.1, 0, -1), 0.5, Dialectric(1.5)))
+world.objects.append(Sphere(Vec3(0, -100, -1), 100, Lambertian(Vec3(0.4, 0.4, 0.4))))
+world.objects.append(Sphere(Vec3(0, 0.8, -1), 0.8, Lambertian(Vec3(0.7, 0.3, 0.2))))
+world.objects.append(Sphere(Vec3(1, 1, -3), 1, Metal(Vec3(0.8, 0.8, 0.8), 0.1)))
+world.objects.append(Sphere(Vec3(0.5, 0.5, 0.4), 0.5, Dialectric(1.5)))
 
 pool = multiprocessing.Pool()
 
